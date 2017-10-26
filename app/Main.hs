be5a3ebb775c393f6ebde9c7d3243c75fba85be7
Dylan Hobbs <dhobbs@tcd.ie>
2017-10-26 14:10:52 +0100

    Basic concurrent structure implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,88 @@
 module Main where
 
-import Lib
+import Network.Socket
+import System.Environment
+import System.IO
+import Control.Concurrent
+import Control.Monad.Fix (fix)
+import Control.Exception
+import Control.Monad (when)
 
-main :: IO ()
-main = someFunc
+type Msg = (Int, String)
+
+main :: IO()
+main = do
+    args <- getArgs
+    let port = getPort args
+    sock <- socket AF_INET Stream 0
+    setSocketOption sock ReuseAddr 1
+    bind sock (SockAddrInet port iNADDR_ANY)
+    listen sock 2
+    chan <- newChan
+
+    _ <- forkIO $ fix $ \loop -> do
+      (_,_) <- readChan chan
+      loop
+
+    mainLoop sock chan 0
+
+getPort :: [String] -> PortNumber
+getPort a = read (head a) :: PortNumber
+
+
+mainLoop :: Socket -> Chan Msg ->  Int -> IO ()
+mainLoop sock chan msgNum = do
+    conn <- accept sock
+    forkIO (runConn conn chan msgNum)
+    mainLoop sock chan $! msgNum + 1
+
+--runConn :: (Socket, SockAddr) -> IO ()
+                --runConn (sock, _) = do
+                --    send sock "Hello World!\n"
+                --    close sock
+
+runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
+runConn (sock, _) chan msgNum = do
+    let broadcast msg = writeChan chan (msgNum, msg)
+    handle1 <- socketToHandle sock ReadWriteMode
+    hSetBuffering handle1 NoBuffering
+
+    hPutStr handle1 "Enter username: "
+    name <- fmap init (hGetLine handle1)
+    broadcast ("---> " ++ name ++ " joined the channel")
+    hPutStrLn handle1 ("Welcome, " ++ name ++ "!")
+
+    commLine <- dupChan chan
+
+    reader <- forkIO $ fix $ \loop -> do
+        (nextNum, line) <- readChan commLine
+        when (msgNum /= nextNum) $ hPutStrLn handle1 line
+        loop
+
+    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+        line <-fmap init (hGetLine handle1)
+        case line of
+          "quit" -> hPutStrLn handle1 "Bye!"
+          "Hello text" -> hPutStrLn handle1  "HELO text\nIP:[ip address]\nPort:[port number]\nStudentID:[your student ID]\n" >> loop
+          _      -> broadcast (name ++ ": " ++ line) >> loop
+
+    killThread reader
+    broadcast ("<--- " ++ name ++ "left the channel")
+    hClose handle1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

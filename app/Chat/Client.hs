f2cb241475562308045c5dd0853e40d5bdaed863
Dylan Hobbs <dhobbs@tcd.ie>
2017-11-09 16:33:15 +0000

    Leaving chatrooms working without JoinID. Using client lookup

--- app/Chat/Client.hs
+++ app/Chat/Client.hs
@@ -39,90 +39,105 @@ gogoClient Server{..} client@Client{..} client_ID = do
 --      forM_ (Map.keys chans) $ \name ->
 --          handleMessage (Disconnect body)
     where
-    readCommands = forever $ do
-        print ("Client: " ++ show client_ID ++ " is waiting for commands")
-        command <- hGetLine clientHandle
-        command <- fmap parseCommand (hGetLine clientHandle)
-        case command of
-          Just (HelloText "text") ->
-                            hPutStrLn clientHandle ("HELO text\nIP: 0" ++ "\nPort: 0"  ++ "\nStudentID: 12301730\n")
-          Just (JoinRequest body) -> do
-                            let x = parseRequest 4 body
-                            let [chatroom_name, client_ip, port, client_name] = x
-                            joinChatroom chatroom_name clientName
-                            print (client_name ++ "joined room: " ++ chatroom_name)
-                            --hPutStrLn clientHandle ("Chatroom: " ++ chatroom_name ++ " Client IP: " ++ client_ip ++ " Port: " ++ port ++ " Client Name: " ++ client_name)
-          Just (LeaveRequest body) -> do
-                            let x = parseRequest 3 body
-                            let [room_ref, join_id, client_name] = x
-                            hPutStrLn clientHandle ("Room: " ++ room_ref ++ " Join_id: " ++ join_id ++ " Client Name: " ++ client_name)
-          Just (Disconnect body) -> do
-                            let x = parseRequest 3 body
-                            let [client_ip, port, client_name] = x
-                            hPutStrLn clientHandle ("Client IP: " ++ client_ip ++ " Port: " ++ port ++ " Client Name: " ++ client_name)
-          Just (MessageSend body) -> do
-                            let x = parseRequest 4 body
-                            let [room_ref, join_id, client_name, message] = x
-                            print ("MESSAGE: " ++ client_name ++ "-> " ++ room_ref)
-                            sendMessage room_ref client_name message
-                            --hPutStrLn clientHandle ("Room Ref: " ++ room_ref ++ " Join ID: " ++ join_id ++ " Client Name: " ++ client_name ++ " Message: " ++ message)
-          Just Terminate ->  hPutStrLn clientHandle "Terminating Server"
-          _      -> hPutStrLn clientHandle "Command not recongnised"
+      readCommands = forever $ do
+          print ("Client: " ++ show client_ID ++ " is waiting for commands")
+          command <- hGetLine clientHandle
+          command <- fmap parseCommand (hGetLine clientHandle)
+          case command of
+            Just (HelloText "text") ->
+                              hPutStrLn clientHandle ("HELO text\nIP: 0" ++ "\nPort: 0"  ++ "\nStudentID: 12301730\n")
+            Just (JoinRequest body) -> do
+                              let x = parseRequest 4 body
+                              let [chatroom_name, client_ip, port, client_name] = x
+                              joinChatroom chatroom_name clientName
+                              print (client_name ++ "joined room: " ++ chatroom_name)
+                              --hPutStrLn clientHandle ("Chatroom: " ++ chatroom_name ++ " Client IP: " ++ client_ip ++ " Port: " ++ port ++ " Client Name: " ++ client_name)
+            Just (LeaveRequest body) -> do
+                              let x = parseRequest 3 body
+                              let [room_ref, join_id, client_name] = x
+                              leaveChatroom room_ref client_name
+                              print (client_name ++ " left room: " ++ room_ref)
+                              --hPutStrLn clientHandle ("Room: " ++ room_ref ++ " Join_id: " ++ join_id ++ " Client Name: " ++ client_name)
+            Just (Disconnect body) -> do
+                              let x = parseRequest 3 body
+                              let [client_ip, port, client_name] = x
+                              hPutStrLn clientHandle ("Client IP: " ++ client_ip ++ " Port: " ++ port ++ " Client Name: " ++ client_name)
+            Just (MessageSend body) -> do
+                              let x = parseRequest 4 body
+                              let [room_ref, join_id, client_name, message] = x
+                              print ("MESSAGE: " ++ client_name ++ "-> " ++ room_ref)
+                              sendMessage room_ref client_name message
+                              --hPutStrLn clientHandle ("Room Ref: " ++ room_ref ++ " Join ID: " ++ join_id ++ " Client Name: " ++ client_name ++ " Message: " ++ message)
+            Just Terminate ->  hPutStrLn clientHandle "Terminating Server"
+            _      -> hPutStrLn clientHandle "Command not recongnised"
 
 
---        handleMessage (Tell channelName msg) _ = atomically $ do
---              channelMap <- readTVar serverChannels
---              case Map.lookup channelName channelMap of
---                Just channel -> tellMessage channel $ TellReply channelName clientUser msg
---                Nothing      -> return ()
+  --        handleMessage (Tell channelName msg) _ = atomically $ do
+  --              channelMap <- readTVar serverChannels
+  --              case Map.lookup channelName channelMap of
+  --                Just channel -> tellMessage channel $ TellReply channelName clientUser msg
+  --                Nothing      -> return ()
 
-    run :: Int -> IO ()
-    run id = forever $ do
-      --print (show id ++ " is running away with it")
-      r <- try . atomically $ do
-        chans <- readTVar connectedChannels
-        foldr (orElse . readTChan) retry
-         $ Map.elems chans
-      case r of
-        Left (e :: SomeException) -> print "derp"
-        Right message -> deliverMessage client message
+      run :: Int -> IO ()
+      run id = forever $ do
+        --print (show id ++ " is running away with it")
+        r <- try . atomically $ do
+          chans <- readTVar connectedChannels
+          foldr (orElse . readTChan) retry
+           $ Map.elems chans
+        case r of
+          Left (e :: SomeException) -> print "derp"
+          Right message -> deliverMessage client message
 
-    sendMessage room name message = atomically $ do
-        channelMap <- readTVar serverChannels
-        case Map.lookup room channelMap of
-            Just chan -> writeTChan (channelChan chan) (Text message)
-            Nothing -> return ()
+      leaveChatroom room client = atomically $ do
+          serverChannelMap <- readTVar serverChannels
+          clientChannelMap <- readTVar connectedChannels
+          case Map.lookup room serverChannelMap of
+              Just (channel@Channel{..}) -> do
+                userSet <- readTVar channelUsers
+                modifyTVar' channelUsers $ Set.delete clientId
+                return channel
+          modifyTVar' connectedChannels $ Map.delete room
 
-
-    deliverMessage :: Client -> Message -> IO ()
-    deliverMessage Client {..} message = do
-         print "Message delivaer"
-         case message of
-           Text body -> printToHandle clientHandle body
-           _ -> printToHandle clientHandle "Could not read message"
-
-    joinChatroom room name = atomically $ do
-         clientChannelMap <- readTVar connectedChannels
-         unless (Map.member room clientChannelMap) $ do
-            channelMap <- readTVar serverChannels
-            channel@Channel{channelChan} <-
-              case Map.lookup room channelMap of
-                  Just (channel@Channel{channelUsers}) -> do
-                    modifyTVar' channelUsers $ Set.insert clientId
-                    return channel
-                  Nothing                             -> do
-                    channel <- newChannel room $ Set.singleton clientId
-                    modifyTVar' serverChannels $ Map.insert room channel
-                    return channel
-            client_chans <- dupTChan channelChan
-            modifyTVar' connectedChannels $ Map.insert room client_chans
+      sendMessage room name message = atomically $ do
+          channelMap <- readTVar serverChannels
+          case Map.lookup room channelMap of
+              Just chan -> writeTChan (channelChan chan) (Text message)
+              Nothing -> return ()
 
 
+      deliverMessage :: Client -> Message -> IO ()
+      deliverMessage Client {..} message = do
+           print "Message delivaer"
+           case message of
+             Text body -> printToHandle clientHandle body
+             _ -> printToHandle clientHandle "Could not read message"
 
-    deepParse :: String -> String
-    deepParse a = splitOn ":" a !! 1
+      joinChatroom room name = atomically $ do
+           clientChannelMap <- readTVar connectedChannels
+           unless (Map.member room clientChannelMap) $ do
+              channelMap <- readTVar serverChannels
+              channel@Channel{channelChan} <-
+                case Map.lookup room channelMap of
+                    Just (channel@Channel{channelUsers}) -> do
+                      modifyTVar' channelUsers $ Set.insert clientId
+                      return channel
+                    Nothing                             -> do
+                      channel <- newChannel room $ Set.singleton clientId
+                      modifyTVar' serverChannels $ Map.insert room channel
+                      return channel
+              client_chans <- dupTChan channelChan
+              modifyTVar' connectedChannels $ Map.insert room client_chans
 
-    parseRequest :: Int -> String -> [String]
-    parseRequest n body = map deepParse (take n (splitOn "\\n" ("first: " ++ body)))
+--      leaveChatroom room name = do
+--           clientChannelMap <- readTVar connectedChannels
+--           print room
+--           --modifyTVar' connectedChannels $ Map.remove room clientChannelMap
+
+      deepParse :: String -> String
+      deepParse a = splitOn ":" a !! 1
+
+      parseRequest :: Int -> String -> [String]
+      parseRequest n body = map deepParse (take n (splitOn "\\n" ("first: " ++ body)))
 
 
